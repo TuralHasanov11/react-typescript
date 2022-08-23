78a5f44acd0601150dbf83d3e6d15985ec2995a5
unknown <hasanovtural11@gmail.com>
2022-08-23 22:17:30 +0400

    project init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+function App() {
+
+  const [count, setCount] = useState()
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
